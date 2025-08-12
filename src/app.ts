1127a9168f6e8e0893383b606ab5556afae1882d
chaudhary-rishabh <chaudharyrishabh029@gmail.com>
2025-08-12 10:52:25 +0530

    added global request limit by IP

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,23 @@ import cors from 'cors';
 import helmet from 'helmet';
 import passport from 'passport';
 import routes from './routes/index';
-import './config/passport'; // Initialize passport strategies
-
+import './config/passport';
+import rateLimit
+ from 'express-rate-limit';
 const app: Application = express();
+
+const globalLimiter = rateLimit({
+    windowMs: 30 * 60 * 1000, 
+    max: 50,
+    message: {
+        success: false,
+        message: 'Too many requests from this IP, please try again after 15 minutes'
+    },
+    standardHeaders: true, 
+    legacyHeaders: false
+});
+
+app.use(globalLimiter);
 
 app.use(helmet());
 app.use(cors({ origin: true, credentials: true }));
@@ -15,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
-app.use('/api', routes);
+app.use('/api/v1', routes);
 
 // Error handler (basic)
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
